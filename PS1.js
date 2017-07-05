cd9d0b0ed69762cdfdf00b6ec0adb2382ad5205e
Justin Kaseman <justinkaseman@live.com>
2017-07-05 13:07:53 -0700

    Finished PS1

--- PS1.js
+++ PS1.js
@@ -1,43 +1,81 @@
-//Hello
-//Implement a program that prints out a simple greeting to the user
-function initHello(){
-  alert("Hello World!");
-}
-
-//Water
-//Implement a program that reports a user’s water usage, converting minutes spent in the shower to bottles of drinking water.
-function initWater(){
-  let sMinutes = prompt("Enter length of your shower in minutes", "");
-  sMinutes;
-  if (sMinutes === null || isNaN(sMinutes)) {
-      return "wrong input";
-    } else
-      alert("You used " + (sMinutes * 12).toFixed(0) + " bottles of water!");
+//************PS1***************
+  //Hello
+  //Implement a program that prints out a simple greeting to the user
+  function initHello(){
+    alert("Hello World!");
   }
 
-//Mario
-//Implement a program that prints out a double half-pyramid of a specified height
-function initMario(){
-
-  let pyramid = "";
-
-  function buildP(h) {
-    for (i = 1; i <= h; i++) {
-      pyramid += " ".repeat(h - i) + "#".repeat(i) + "  " + "#".repeat(i) + "\n";
+  //Water
+  //Implement a program that reports a user’s water usage, converting minutes spent in the shower to bottles of drinking water.
+  function initWater(){
+    let sMinutes = prompt("Enter length of your shower in minutes", "");
+    sMinutes;
+    if (sMinutes === null || isNaN(sMinutes)) {
+        return "wrong input";
+      } else
+        alert("You used " + (sMinutes * 12).toFixed(0) + " bottles of water!");
     }
-    console.log(pyramid)
+
+  //Mario
+  //Implement a program that prints out a double half-pyramid of a specified height
+  function initMario(){
+
+    let pyramid = "";
+
+    function buildP(h) {
+      for (i = 1; i <= h; i++) {
+        pyramid += " ".repeat(h - i) + "#".repeat(i) + "  " + "#".repeat(i) + "\n";
+      }
+      console.log(pyramid)
+    }
+
+    while (true) {
+      let pHeight = prompt("Enter the height of the pyramid for Mario to jump");
+      if (pHeight < 24 && pHeight >= 0) {
+          buildP(pHeight);
+          break;
+        } else {
+            alert("Height must be between 0 and 23. Try Again!");
+          }
+    }
+  }
+
+  //Greedy
+  //Implement a program that calculates the minimum number of coins required to give a user change.
+function initGreedy(){
+
+let q = 0
+    ,d = 0
+    ,n = 0
+    ,p = 0;
+
+  function getChange(changeAmount) {
+    if (changeAmount > 25) {
+      q = (changeAmount / 25).toFixed(0);
+      changeAmount %= 25;
+    } if (changeAmount > 10) {
+        d = (changeAmount / 10).toFixed(0);
+        changeAmount %= 10;
+      } if (changeAmount > 5) {
+          n = (changeAmount / 5).toFixed(0);
+          changeAmount %= 5;
+        } if (changeAmount > 1) {
+          p = (changeAmount / 1).toFixed(0);
+          }
+          alert(q + " quarters, " + d + " dimes, " + n + " nickles, " + p + " pennies.");
   }
 
   while (true) {
-    let pHeight = prompt("Enter the height of the pyramid for Mario to jump");
-    if (pHeight < 24 && pHeight >= 0) {
-        buildP(pHeight);
-        break;
-      } else {
-          alert("Height must be between 0 and 23. Try Again!");
-        }
+    let changeAmount = prompt("How much change is owed?");
+    if (changeAmount >= 0) {
+      changeAmount *= 100;
+      getChange(changeAmount);
+      break;
+    } else {
+          alert("You can't have negative change. Try again!");
+      }
   }
+
 }
 
-//Credit
-//Implement a program that determines whether a provided credit card number is valid according to Luhn’s algorithm.
+//************PS2***************
--- README.md
+++ README.md
@@ -4,8 +4,32 @@ Harvard's CS50 found here:
   Introduction:
       This is CS50x, Harvard University's introduction to the intellectual enterprises of computer science and the art of programming for majors and non-majors alike, with or without prior programming experience. An entry-level course taught by David J. Malan, CS50x teaches students how to think algorithmically and solve problems efficiently. Topics include abstraction, algorithms, data structures, encapsulation, resource management, security, software engineering, and web development. Languages include C, PHP, and JavaScript plus SQL, CSS, and HTML. Problem sets inspired by real-world domains of biology, cryptography, finance, forensics, and gaming. As of Fall 2015, the on-campus version of CS50x, CS50, was Harvard's largest course.
 
+    Problem Set 0
+      Scratch
+      Scratch
+    Problem Set 1
+      C
+      C
+    Problem Set 2
+      Crypto
+      C
+    Problem Set 3
+      Game of Fifteen
+      C
+    Problem Set 4
+      Forensics
+      C
+    Problem Set 5
+      Misspellings
+      C
+    Problem Set 6
+      Sentimental
+      Python
+    Problem Set 7
+      C$50 Finance
+      Python, SQL
+    Problem Set 8
+      Mashup
+      JavaScript
 
-
-Problem Sets are taught in C
-
-My Solutions will be written in ES6Script as to practice JavaScript
+My Solutions will all be written in ES6Script as to practice JavaScript
